6ba15e40d56ef02b5bee566adad0fde6c6cd7621
drhardian <doni.hardian@yahoo.com>
2023-07-05 00:52:44 +0700

    add modal form

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -5,64 +5,122 @@
         <div class="p-4 mt-2">
             {{-- <aside
                 class="p-4 my-8 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700"> --}}
-                <h3 class="mb-2 hidden md:block text-2xl font-medium text-gray-900 dark:text-white">{{ $title }}</h3>
+            <h3 class="mb-2 hidden md:block text-2xl font-medium text-gray-900 dark:text-white">{{ $title }}</h3>
 
-                @unless (count($breadcrumbs) === 0)
-                    <div class="flex justify-between">
-                        <nav class="flex" aria-label="Breadcrumb">
-                            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-                                @foreach ($breadcrumbs as $breadcrumb)
-                                    <li class="inline-flex items-center">
-                                        @if ($breadcrumb['status'] === 'active')
-                                            <a href="{{ $breadcrumb['url'] }}"
-                                                class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                                                <i class="{{ $breadcrumb['icon'] }} mr-2"></i>
-                                                {{ $breadcrumb['title'] }}
-                                            </a>
-                                        @else
-                                            <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor"
-                                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd"
-                                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                                    clip-rule="evenodd"></path>
-                                            </svg>
-                                            <span
-                                                class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">{{ $breadcrumb['title'] }}</span>
-                                        @endif
-                                    </li>
-                                @endforeach
-                            </ol>
-                        </nav>
-                        <button type="button" class="text-white bg-blue-700 hidden sm:block hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <i class="fa-solid fa-plus mr-2"></i>New
-                        </button>
-                    </div>
-                @endunless
-                <div class="mt-7">
-                    <table id="customer-table" class="table table-striped table-bordered" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Address</th>
-                                <th>Phone</th>
-                                <th>Email</th>
-                                <th>Last Update</th>
-                            </tr>
-                        </thead>
-                        <tbody></tbody>
-                    </table>
+            @unless (count($breadcrumbs) === 0)
+                <div class="flex justify-between">
+                    <nav class="flex" aria-label="Breadcrumb">
+                        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                            @foreach ($breadcrumbs as $breadcrumb)
+                                <li class="inline-flex items-center">
+                                    @if ($breadcrumb['status'] === 'active')
+                                        <a href="{{ $breadcrumb['url'] }}"
+                                            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                            <i class="{{ $breadcrumb['icon'] }} mr-2"></i>
+                                            {{ $breadcrumb['title'] }}
+                                        </a>
+                                    @else
+                                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor"
+                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                clip-rule="evenodd"></path>
+                                        </svg>
+                                        <span
+                                            class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">{{ $breadcrumb['title'] }}</span>
+                                    @endif
+                                </li>
+                            @endforeach
+                        </ol>
+                    </nav>
+                    <button type="button"
+                        class="text-white bg-blue-700 hidden sm:block hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        data-modal-target="newCustomerModal" data-modal-toggle="newCustomerModal">
+                        <i class="fa-solid fa-plus mr-2"></i>New
+                    </button>
                 </div>
+            @endunless
+            <div class="mt-7">
+                <table id="customer-table" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>Last Update</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
             {{-- </aside> --}}
         </div>
     </div>
-    <button class="flex sm:hidden fixed z-50 bottom-10 right-8 bg-blue-700 w-12 h-12 rounded-full drop-shadow-lg justify-center items-center text-white text-xl hover:bg-blue-800 hover:drop-shadow-2xl animate-bounce duration-300">
+    <button
+        class="flex sm:hidden fixed z-50 bottom-10 right-8 bg-blue-700 w-12 h-12 rounded-full drop-shadow-lg justify-center items-center text-white text-xl hover:bg-blue-800 hover:drop-shadow-2xl animate-bounce duration-300"
+        data-modal-target="newCustomerModal" data-modal-toggle="newCustomerModal">
         <i class="fa-solid fa-plus"></i>
     </button>
+
+
+    <!-- New Customer modal -->
+    <div id="newCustomerModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-2xl max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white modal-title"></h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="newCustomerModal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="mb-6">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                email</label>
+                            <input type="email" id="email"
+                                class="bg-gray-50 sm:p-2 p-1.5 border border-gray-300 text-gray-900 sm:text-base text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="name@flowbite.com" required>
+                        </div>
+                        <div class="mb-6">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                password</label>
+                            <input type="password" id="password"
+                                class="bg-gray-50 sm:p-2 p-1.5 border border-gray-300 text-gray-900 sm:text-base text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required>
+                        </div>
+                    </form>
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
+                        accept</button>
+                    <button data-modal-hide="newCustomerModal" type="button"
+                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 @section('js')
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             $('#customer-table').DataTable({
                 language: {
                     processing: "Loading. Please wait..."
@@ -74,12 +132,31 @@
                 ajax: {
                     url: "{{ route('client.main.table') }}",
                 },
-                columns: [
-                    { data: 'name', name: 'name', className: 'all' },
-                    { data: 'address', name: 'address', className: 'min-tablet' },
-                    { data: 'phone_number', name: 'phone_number', class: ['text-center','min-tablet'] },
-                    { data: 'email', name: 'email', className: 'min-tablet' },
-                    { data: 'updated_at', name: 'updated_at', class: ['text-center','min-tablet'] },
+                columns: [{
+                        data: 'name',
+                        name: 'name',
+                        className: 'all'
+                    },
+                    {
+                        data: 'address',
+                        name: 'address',
+                        className: 'min-tablet'
+                    },
+                    {
+                        data: 'phone_number',
+                        name: 'phone_number',
+                        class: ['text-center', 'min-tablet']
+                    },
+                    {
+                        data: 'email',
+                        name: 'email',
+                        className: 'min-tablet'
+                    },
+                    {
+                        data: 'updated_at',
+                        name: 'updated_at',
+                        class: ['text-center', 'min-tablet']
+                    },
                 ]
             });
         });
