262d796943aa5e4b2d73e0526aa15d56fb5e8cd9
doni.hardian <doni.hardian@yahoo.com>
2024-03-20 06:43:26 +0700

    update kernel auth

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -119,11 +119,18 @@
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                 <ul
                     class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <!-- Home -->
+                    <li>
+                        <a href="home"
+                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
+                    </li>
                     <!-- Asset Manager -->
+                    @can('asset_manager')
                     <li>
                         <a href="https://cms.donihardian.my.id" target="_blank"
                             class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Asset Manager</a>
                     </li>
+                    @endcan
                     <!-- Maintenance Manager -->
                     <li>
                         <a href="{{route('valverepair.index')}}"
